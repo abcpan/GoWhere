d139002443cfd910637e861988d6cb024610d817
pan <1663071425@qq.com>
2018-08-03 10:07:22 +0800

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<!-- 模板 -->
+<template>
+	<!-- template对外只能暴露一个根页面标签 -->
+	<div>
+		hello, world
+	</div>
+</template>
+<!-- 模板的逻辑 -->
+<script>
+export default{
+  name: 'Home'
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/home/Home'
+import List from '@/pages/list/List'
 
 Vue.use(Router)
 
@@ -8,8 +9,12 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home', // 组件的名称
+      component: Home // 组件的文件名
+    }, {
+      path: '/list',
+      name: 'List',
+      component: List
     }
   ]
 })
