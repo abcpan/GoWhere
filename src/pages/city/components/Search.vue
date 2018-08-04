1aa08b4c3627ddd906c9db657cf7d6321b2490f6
pan <1663071425@qq.com>
2018-08-05 00:39:09 +0800

    finished search of city

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="search">
+		<input class="search-input" type="text" placeholder="输入城市名或者拼音">
+	</div>
+</template>
+
+<script>
+	export default{
+		name: 'CitySearch'
+	}
+</script>
+
+<style lang="stylus" scoped>
+	@import '~styles/varibles.styl'
+	.search
+		height: .72rem
+		padding: 0 .1rem
+		background: $bgColor
+		.search-input
+			width: 100%
+			box-sizing: border-box
+			margin-top:.1rem
+			
+			height: .5rem
+			line-height: .5rem
+			border-radius: .06rem
+			padding: 0 .2rem
+			text-align: center
+			color: #666
+</style>
