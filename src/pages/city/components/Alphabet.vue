3c17abc0cf74a1d51446007ead86af5e01572fc0
pan <1663071425@qq.com>
2018-08-05 11:28:50 +0800

    completed the city page

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,34 @@
+<template>
+	<ul class="list">
+		<li class="item">h</li>
+		<li class="item">h</li>
+		<li class="item">h</li>
+		<li class="item">h</li>
+		<li class="item">h</li>
+		<li class="item">h</li>
+	</ul>
+</template>
+
+<script>
+	export default{
+		name: 'CityAlphabet'
+	}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+	.list
+		display:flex
+		flex-direction:column  // 垂直排列
+		justify-content: center  // 垂直排列后居中
+		position: absolute
+		right: 0
+		top: 1.58rem
+		bottom: 0
+		width: .4rem
+		.item
+			text-align:center
+			line-height: .44rem
+			color:$bgColor
+	
+</style>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,126 @@
+<template>
+	<div class="list" ref="wrapper">
+		<div>
+			<div class="area">
+				<div class="area-title border-topbottom">当前城市</div>
+				<div class="button-list">
+					<div class="button-wrapper">
+						<div class="button">北京</div>
+					</div>
+				</div>
+			</div>
+			<div class="area">
+				<div class="area-title border-topbottom">热门城市</div>
+				<div class="button-list">
+					<div class="button-wrapper">
+						<div class="button">北京</div>
+					</div>
+					<div class="button-wrapper">
+						<div class="button">北京</div>
+					</div>
+					<div class="button-wrapper">
+						<div class="button">北京</div>
+					</div>
+					<div class="button-wrapper">
+						<div class="button">北京</div>
+					</div>
+					<div class="button-wrapper">
+						<div class="button">北京</div>
+					</div>
+					<div class="button-wrapper">
+						<div class="button">北京</div>
+					</div>
+				</div>
+			</div>
+			<div class="area">
+				<div class="area-title border-topbottom">A</div>
+				<div class="item-list">
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+				</div>
+			</div>
+			<div class="area">
+				<div class="area-title border-topbottom">A</div>
+				<div class="item-list">
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+				</div>
+			</div>
+			<div class="area">
+				<div class="area-title border-topbottom">A</div>
+				<div class="item-list">
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	import Bscroll from 'better-scroll'
+	export default {
+		name: 'CityList',
+		mounted: function() {
+			this.scroll = new Bscroll(this.$refs.wrapper)
+		}
+	}
+</script>
+
+<style lang="stylus" scoped>
+// 公共样式
+	.border-topbottom
+		/*设上下边框*/
+		&:before
+			border-color: #ccc
+		&:after
+			border-color: #ccc
+	.border-bottom
+		&:after
+			border-color: #ccc
+			/*私有样式*/
+	.list
+		overflow: hidden
+		position:absolute
+		top:1.58rem 
+		left: 0
+		right:0
+		bottom: 0
+		.area-title
+			line-height: .54rem
+			background:#eee
+			padding-left: .2rem
+			color: #666
+			font-size: .26rem
+		.button-list
+			overflow: hidden
+			padding: .1rem
+			.button-wrapper
+				float: left  /*本身元素进行漂浮*/
+				width: 33.3%
+			.button
+				text-align: center
+				margin: .1rem
+				border: .02rem solid #ccc
+				padding: .1rem .6rem .1rem .6rem
+		.item-list
+			.item
+				line-height: .76rem
+				padding-left: .2rem
+	
+</style>
